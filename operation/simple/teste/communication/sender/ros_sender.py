49eb4777b6ad46800824add508b2d92640e1fc88
argenton <joaopedroargenton@hotmail.com>
2024-11-11 16:47:49 -0300

    atuaizaçao sender para 3 topicos ROS HALT concluido

--- operation/simple/teste/communication/sender/ros_sender.py
+++ operation/simple/teste/communication/sender/ros_sender.py
@@ -3,16 +3,25 @@ from geometry_msgs.msg import Twist
 
 from .sender import Sender
 
+
 class RosSender(Sender):
     def __init__(self):
-        rospy.init_node('keyboard_control')
+        
+        rospy.init_node('bob_joga_bola')
 
         self.pub_robot1 = rospy.Publisher('/bob1/cmd_vel', Twist, queue_size=10)
+        self.pub_robot2 = rospy.Publisher('/bob2/cmd_vel', Twist, queue_size=10)
+        self.pub_robot3 = rospy.Publisher('/bob3/cmd_vel', Twist, queue_size=10)
 
     def transmit_robot(self, robot_id, left_speed, right_speed):
         twist = Twist()
-        twist.linear.x = right_speed
-        twist.linear.y = left_speed
-        twist.linear.z = robot_id
+        twist.linear.x = -right_speed
+        twist.linear.y = -left_speed
 
-        self.pub_robot1.publish(twist)
+        pub_map = {
+            1: self.pub_robot3,
+            2: self.pub_robot1,
+            0: self.pub_robot2
+        }
+
+        pub_map[robot_id].publish(twist)
