83951c15075320ef13be5ff47b02f788efb4d889
victorhac <victorhac231@gmail.com>
2024-11-12 23:42:31 -0300

    teste

--- operation/simple/teste/teste_comunicacao_bluetooth_servidor.py
+++ operation/simple/teste/teste_comunicacao_bluetooth_servidor.py
@@ -1,26 +1,22 @@
 import socket
-
+import pickle
 from domain.field import Field
 
 server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-
 server.bind(("5C:CD:5B:40:9B:7D", 4))
-
 server.listen(1)
 
 client, addr = server.accept()
-
 print(f"Conexão recebida de {addr}")
 
 while True:
     try:
-        data = client.recv(1024)
+        data = client.recv(2048)
         if not data:
             break
-        field = Field()
-
-        field.load_from_dict(data)
-
+        
+        field = pickle.loads(data)
+        
         print(f"Recebido: {field}")
     except Exception as e:
         print(f"Erro ao receber dados: {e}")
