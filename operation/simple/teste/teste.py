d8204ae64d94c9af98b267e1268b064ae6fe54c8
victorhac <victorhac231@gmail.com>
2024-11-13 14:15:06 -0300

    teste do receiver

--- operation/simple/teste/teste.py
+++ operation/simple/teste/teste.py
@@ -1,12 +1,13 @@
+from communication.sender.game_controller.game_controller_remote_sender import GameControllerRemoteSender
 from communication.sender.ssl_vision.ssl_vision_remote_sender import SSLVisionRemoteSender
 
 
 class Teste:
     def __init__(self):
-        self.ssl_vision_remote_sender = SSLVisionRemoteSender()
+        self.game_controller_remote_sender = GameControllerRemoteSender()
 
     def main(self):
-        self.ssl_vision_remote_sender.main()
+        self.game_controller_remote_sender.main()
 
 def main():
     teste = Teste()
